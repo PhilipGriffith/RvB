87f6ae44a6d310d3fe51d9b69cdeddf88a01a386
philip.griffith <philip.griffith@gmail.com>
2019-04-14 20:53:33 -0400

    Changed everything to classes. Better!

--- main.py
+++ main.py
@@ -1,22 +1,91 @@
-chumps = ('renter', 'buyer')
 
-income = 40000
-savings = 20000
+class Finances:
 
-investment_annual_return_percent = 5.7
-housing_annual_return_percent = 4.7
+    def __init__(self):
+        self.income = 40000
+        self.savings = 20000
+        self.housing_budget_percent = 30
+        self.housing_budget = (self.income / 12) * (self.housing_budget_percent / 100)
 
-rent = 770
-house_value = 200000
-
-# Assuming insurance and property tax rates do not change
-insurance = ((1020 + 1191 + 1390) / 3) / 12
-property_tax = (((0.0089 + 0.00977) / 2) / 12) * house_value
-maintenance = (0.02 * house_value) / 12  # Rule of thumb: 2-5%
+        self.rent = 770
+        self.house_value = 200000
+        self.housing_appreciation_percent = 4.7
 
 
-pmi = 0.005 * (house_value - savings)  # Rule of thumb: 0.5-1% of original loan
-print(pmi / 12)
+class MonthlyHousing(Finances):
+
+    def __init__(self, loan=None):
+        super().__init__()
+        self.payment = loan.payment if loan else self.rent
+        self.utilities = 200 if loan else 90
+        self.maintenance = (0.02 * self.house_value) / 12 if loan else 0  # Rule of thumb: 2-5%
+        # Assuming static insurance and property tax rates
+        self.insurance = ((1020 + 1191 + 1390) / 3) / 12 if loan else 0
+        self.property_tax = (((0.0089 + 0.00977) / 2) / 12) * self.house_value if loan else 0
+        self.cost = sum((self.payment, self.utilities, self.maintenance, self.insurance, self.property_tax))
+
+
+class Loan:
+
+    # Does not consider PMI
+    def __init__(self, amount, years=30):
+        self.amount = amount
+        self.length_in_years = years
+        self.length_in_months = self.length_in_years * 12
+        self.number_of_payments = self.length_in_months
+        self.annual_interest_rate = (sum((4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.19, 4.25, 4.25, 4.375, 5.125)) / 11) / 100
+        self.monthly_interest_rate = self.annual_interest_rate / 12
+        self.payment = self.compute_monthly_payment()
+
+    def compute_monthly_payment(self):
+
+        numerator = ((1 + self.monthly_interest_rate) ** self.length_in_months) - 1
+        denominator = self.monthly_interest_rate * (1 + self.monthly_interest_rate) ** self.length_in_months
+        discount_factor = numerator / denominator
+        return self.amount / discount_factor
+
+    def make_payment(self):
+        interest = self.amount * self.monthly_interest_rate
+        self.amount -= self.payment - interest
+        self.number_of_payments -= 1
+
+    def details(self, v=False):
+        if v:
+            print('Interest rate: ~{:.2f}'.format(self.annual_interest_rate))
+            print('Payment: ~{:.2f}'.format(self.payment))
+        print('Amount:~{:.2f}'.format(self.amount))
+        print('Number of payments:', self.number_of_payments)
+
+
+class Investment:
+
+    def __init__(self, amount):
+        self.annual_rate_percent = 5.7
+        self.monthly_rate = (self.annual_rate_percent / 100) / 12
+        self.commission_rate_percent = 0.5
+        self.fee_rate_percent = 0.2
+        self.amount = amount - (amount * (self.commission_rate_percent / 100))
+
+    def compound(self, amount=0):
+        self.amount += self.amount * self.monthly_rate
+        self.amount -= self.amount * ((self.fee_rate_percent / 100) / 12)
+        self.amount += amount
+
+    def sell(self):
+        return self.amount - (self.amount * (self.commission_rate_percent / 100))
+
+    def details(self):
+        print('Amount: {:.2f}'.format(self.amount))
+
+
+i = Investment(100)
+i.details()
+for t in range(12):
+    i.compound()
+    i.details()
+print(i.sell())
+
+
 
 # Todo Figure this out
 rate_30yr_0_pts = (4.0, 4.0, 4.0, 4.0, 4.0, 4.25, 4.375, 4.0, 4.19, 4.25, 5.125)
@@ -27,31 +96,7 @@ rate_15yr_0_pts = (3.5, 3.625, 3.625, 3.625, 3.5, 3.5, 3.75, 3.625, 3.75, 3.79)
 apr_15yr_0_pts = (3.778, 3.824, 3.847, 3.854, 3.5, 3.584, 3.823, 3.656, 3.75, 3.893)
 fees_15yr_0_pts = (1095, 134, 410, 493, 1, 1150, 995, 383, 0, 1258)
 
-import numpy as np
-print(np.mean(rate_15yr_0_pts))
-
-mortgage = 0
-
-# Per month
-cost = {'renter': rent, 'buyer': mortgage}
-utilities = {'renter': 90, 'buyer': 200}
-maintenance = {'renter': 0, 'buyer': maintenance}
-property_tax = {'renter': 0, 'buyer': property_tax}
-insurance = {'renter': 0, 'buyer': insurance}
-
-
-def compute_monthly_cost(chump):
-
-    costs = cost[chump] + \
-            utilities[chump] + \
-            maintenance[chump] + \
-            property_tax[chump] + \
-            insurance[chump]
-
-    print('Total {} monthly costs: {:.2f}'.format(chump, costs))
-    return costs
-
-
+# todo put this in a mortgage class
 def compute_closing_cost(years=30):
 
     boa_closing_cost_30yr = 9934 - 1712  # Includes 1712 in points
@@ -73,85 +118,10 @@ def compute_closing_cost(years=30):
                 navy_federal_closing_cost_15yr + besmartee_closing_cost_15yr) / 5
 
 
-def compute_monthly_mortgage_cost(loan_amount, number_of_payments, interest_rate_percent):
-    """
-    This computes the monthly mortgage payment for a given loan with n remaining payments at a monthly compounded rate.
-    """
-    interest_rate = interest_rate_percent / 100
-    monthly_interest_rate = interest_rate / 12
-    numerator = ((1 + monthly_interest_rate) ** number_of_payments) - 1
-    denominator = monthly_interest_rate * (1 + monthly_interest_rate) ** number_of_payments
-    discount_factor = numerator / denominator
-
-    return loan_amount / discount_factor
-
-
-def compute_monthly_investment_gain(amount, annual_rate_percent):
-    """
-    This computes the amount gained in a single month given an amount and an annual rate.
-    """
-    monthly_rate = (annual_rate_percent / 100) / 12
-    return amount * monthly_rate
-
-
-def compute_monthly_investment_amount(amount, annual_rate_percent, fee_percent=0.2):
-    """
-    This computes the total investment amount in a single month given an amount and an annual rate,
-    accounting for management fees of 0.2%.
-    """
-    fee = (fee_percent / 100) / 12
-    amount += compute_monthly_investment_gain(amount, annual_rate_percent)
-    amount -= amount * fee
-    return amount
-
-
-def compute_investment_amount(years, initial_amount, added_amount, annual_rate_percent):
-    """
-    This computes the total investment amount in n years given an initial amount, monthly additions,
-    and an annual rate, accounting for management fees of 0.2%.
-    """
-    months = years * 12
-    total_amount = initial_amount
-    for month in range(months):
-        total_amount = compute_monthly_investment_amount(total_amount, annual_rate_percent)
-        total_amount += added_amount
-    return total_amount
-
-
-def compute_total_investment(years, initial_amount, added_amount, annual_rate_percent, commission_percent=0.5):
-    """
-    This may not work for mortgage chump who alters the added amount every month.
-    :param years:
-    :param initial_amount:
-    :param added_amount:
-    :param annual_rate_percent:
-    :param commission_percent:
-    :return:
-    """
-    global savings
-    commission = commission_percent / 100
-    buy_commission = initial_amount * commission
-    print(buy_commission)
-    savings -= buy_commission
-    total_amount = compute_investment_amount(years, initial_amount, added_amount, annual_rate_percent)
-    sell_commission = total_amount * commission
-    print(sell_commission)
-    print(total_amount)
-    total_amount -= sell_commission
-    savings += total_amount
-
-
-    begin = buy_commission + initial_amount
-    print(begin)
-    print('Return', ((total_amount - begin) / begin) / 25)
-
-    return savings
-
-
 if __name__ == '__main__':
 
-    for chump in chumps:
-        compute_monthly_cost(chump)
+    loan = Loan(180000)
 
-    print(compute_total_investment(25, 100, 0, 8) - 20000)
+    mh = MonthlyHousing(loan).cost
+    print(mh)
 
