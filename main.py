30bf0968938763c6a4c9a8d615e1fdd6b6f50701
philip.griffith <philip.griffith@gmail.com>
2019-04-20 01:01:38 -0400

    Added objects and script to compute the difference between rent and mortgage.

--- main.py
+++ main.py
@@ -1,24 +1,66 @@
 
-class Finances:
+class House:
 
     def __init__(self):
-        self.savings = 20000
+        self.price = 200000
+        self.value = self.price
+        self.appreciation_rate_percent = 4.7
+        self.mortgage_price = self.value + self.compute_closing_cost()
+
+    def appreciate(self):
+        self.value += self.value * ((self.appreciation_rate_percent / 100) / 12)
+
+    @staticmethod
+    def compute_closing_cost(years=30):
+
+        boa_closing_cost_30yr = 9934 - 1712  # Includes 1712 in points
+        boa_closing_cost_15yr = 9522 - 1328  # Includes 1328 in points
+        nerdwallet_closing_cost_30yr = 8824  # No location given, no year (difference in interest rate)
+        nerdwallet_closing_cost_15yr = 8786  # No location given, no year (difference in interest rate)
+        smart_asset_closing_cost_30yr = 6314  # Location given
+        smart_asset_closing_cost_15yr = 6286  # Location given
+        navy_federal_closing_cost_30yr = 5018  # No location given
+        navy_federal_closing_cost_15yr = 4980  # No location given
+        besmartee_closing_cost_30yr = 6891  # No location, no year (difference in interest rate)
+        besmartee_closing_cost_15yr = 6850  # No location, no year (difference in interest rate)
+
+        if years == 30:
+            return (boa_closing_cost_30yr + nerdwallet_closing_cost_30yr + smart_asset_closing_cost_30yr +
+                    navy_federal_closing_cost_30yr + besmartee_closing_cost_30yr) / 5
+        elif years == 15:
+            return (boa_closing_cost_15yr + nerdwallet_closing_cost_15yr + smart_asset_closing_cost_15yr +
+                    navy_federal_closing_cost_15yr + besmartee_closing_cost_15yr) / 5
+
+
+class Apartment:
+
+    def __init__(self):
         self.rent = 770
-        self.house_value = 200000
-        self.housing_appreciation_percent = 4.7
+        self.rent_increase_percent = 1.5
+
+    def rate_increase(self):
+        self.rent += self.rent * ((self.rent_increase_percent / 100) / 12)
 
 
-class MonthlyHousing(Finances):
+class MonthlyHousing:
 
-    def __init__(self, loan=None):
+    def __init__(self, loan=None, house_price=None, house_value=None, rent=None):
         super().__init__()
-        self.payment = loan.payment if loan else self.rent
+        self.payment = loan.payment if loan else rent
         self.utilities = 200 if loan else 90
-        self.maintenance = (0.02 * self.house_value) / 12 if loan else 0  # Rule of thumb: 2-5%
+        self.maintenance = (0.02 * house_price) / 12 if loan else 0  # Rule of thumb: 2-5%
         # Assuming static insurance and property tax rates
         self.insurance = ((1020 + 1191 + 1390) / 3) / 12 if loan else 0
-        self.property_tax = (((0.0089 + 0.00977) / 2) / 12) * self.house_value if loan else 0
+        self.property_tax = (((0.0089 + 0.00977) / 2) / 12) * house_value if loan else 0
         self.cost = sum((self.payment, self.utilities, self.maintenance, self.insurance, self.property_tax))
+
+    def details(self):
+        print('Loan payment:', self.payment)
+        print('Utilities:', self.utilities)
+        print('Maintenance:', self.maintenance)
+        print('Insurance:', self.insurance)
+        print('Property tax:', self.property_tax)
+        print('Total:', self.cost)
 
 
 class Loan:
@@ -44,12 +86,14 @@ class Loan:
         interest = self.amount * self.monthly_interest_rate
         self.amount -= self.payment - interest
         self.number_of_payments -= 1
+        if self.number_of_payments == 0:
+            self.payment = 0
 
     def details(self, v=False):
         if v:
             print('Interest rate: ~{:.2f}'.format(self.annual_interest_rate))
             print('Payment: ~{:.2f}'.format(self.payment))
-        print('Amount:~{:.2f}'.format(self.amount))
+        print('Amount: ~{:.2f}'.format(self.amount))
         print('Number of payments:', self.number_of_payments)
 
 
@@ -74,37 +118,32 @@ class Investment:
         print('Amount: {:.2f}'.format(self.amount))
 
 
-# Todo Figure this out
+# "Your monthly payment is based on the interest rate and principal balance, not the APR."
 rate_30yr_0_pts = (4.0, 4.0, 4.0, 4.0, 4.0, 4.25, 4.375, 4.0, 4.19, 4.25, 5.125)
-apr_30yr_0_pts = (4.269, 4.270, 4.289, 4.310, 4.0, 4.299, 4.375, 4.045, 4.249, 4.25, 5.215)
-fees_30yr_0_pts = (234, 253, 641, 1082, 1, 1150, 0, 972, 1258, 0, 1825)
-
 rate_15yr_0_pts = (3.5, 3.625, 3.625, 3.625, 3.5, 3.5, 3.75, 3.625, 3.75, 3.79)
-apr_15yr_0_pts = (3.778, 3.824, 3.847, 3.854, 3.5, 3.584, 3.823, 3.656, 3.75, 3.893)
-fees_15yr_0_pts = (1095, 134, 410, 493, 1, 1150, 995, 383, 0, 1258)
-
-
-def compute_closing_cost(years=30):
-
-    boa_closing_cost_30yr = 9934 - 1712  # Includes 1712 in points
-    boa_closing_cost_15yr = 9522 - 1328  # Includes 1328 in points
-    nerdwallet_closing_cost_30yr = 8824  # No location given, no year (difference in interest rate)
-    nerdwallet_closing_cost_15yr = 8786  # No location given, no year (difference in interest rate)
-    smart_asset_closing_cost_30yr = 6314  # Location given
-    smart_asset_closing_cost_15yr = 6286  # Location given
-    navy_federal_closing_cost_30yr = 5018  # No location given
-    navy_federal_closing_cost_15yr = 4980  # No location given
-    besmartee_closing_cost_30yr = 6891  # No location, no year (difference in interest rate)
-    besmartee_closing_cost_15yr = 6850  # No location, no year (difference in interest rate)
-
-    if years == 30:
-        return (boa_closing_cost_30yr + nerdwallet_closing_cost_30yr + smart_asset_closing_cost_30yr +
-                navy_federal_closing_cost_30yr + besmartee_closing_cost_30yr) / 5
-    elif years == 15:
-        return (boa_closing_cost_15yr + nerdwallet_closing_cost_15yr + smart_asset_closing_cost_15yr +
-                navy_federal_closing_cost_15yr + besmartee_closing_cost_15yr) / 5
 
 
 if __name__ == '__main__':
 
-    pass
+    balance = 20000
+    years = 50
+
+    # 30-year Mortgage
+    house = House()
+    loan_amount = house.mortgage_price - balance
+    loan = Loan(loan_amount)
+    balance = 0
+
+    # Todo Add investment
+    apartment = Apartment()
+
+    for month in range(years * 12):
+
+        house_cost = MonthlyHousing(loan, house.price, house.value).cost
+        rent_cost = MonthlyHousing(rent=apartment.rent).cost
+        difference = house_cost - rent_cost
+        if loan.number_of_payments > 0:
+            loan.make_payment()
+        house.appreciate()
+        apartment.rate_increase()
+    # print(house.value)
